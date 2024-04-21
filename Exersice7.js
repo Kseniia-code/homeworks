8bc841e1811a29f26c61b66b016f1bd61fc15a7a
Kseniya Gorbunova <kseniya.gorbunova14@gmail.com>
2024-04-21 13:31:27 +0100

    New changes

--- Exersice7.js
+++ Exersice7.js
@@ -1,8 +1,19 @@
 function pow(x, y) {                   //function declaration
+    let result = x;
     for (let i = 1; i < y; i++) {      //loop to repeat operation "y" times 
         result = result * x;           // repeatable multiplication of the previous operation result and initial "x" value
     }
 
-    console.log(result);
+    console.log("Actual result = " + result);
+    let expectedResult = x**y;
+    console.log("Expected result = " + expectedResult);
+    if(expectedResult == result){    
+    console.log("Actual result is equal to expected");
+    }
+    else
+    {
+    console.log("Actual result does not match to expected");
+    }
+
 }
 pow(2, 4);                            //function invocation
