666a31f13c2fb2efcb4aef30bebca4e0ed19aef2
Kseniya Gorbunova <kseniya.gorbunova14@gmail.com>
2024-05-05 13:50:47 +0100

    js_homework13

---
+++ js_homework13.js
@@ -0,0 +1,14 @@
+//Задаємо числовий масив "example".
+let example = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+function myBlend(arr) {
+    for (let i = 0; i <= arr.length * 2; i++) {             //Цикл for для повторення операції зміни позиції елементу масива
+        let firstElementPosition = Math.floor(Math.random() * (arr.length - 0) + 0);      //Обчислення рандомної позиції елементу масива
+        let secondElementPosition = Math.floor(Math.random() * (arr.length - 0) + 0);
+        const firstElement = arr[firstElementPosition];     //Зберігаємо значення елемента масиву в змінну
+        const secondElement = arr[secondElementPosition];
+        arr[firstElementPosition] = secondElement;      //Міняємо місцями перший та другий елементи масива
+        arr[secondElementPosition] = firstElement;
+    }
+}
+myBlend(example);
+console.log(example);
